502fdcadd77a407c56ac7d1e7a251e544ea21b84
Jariullah Safi <jari@localhost.localdomain>
2016-09-19 19:45:47 -0400

    autocomplete plugin

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -8,5 +8,8 @@ fi
 # Install oh my zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+#Install the autocomplete suggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
 # Copy the .zshrc file
 cp zshrc /home/$USER/.zshrc
